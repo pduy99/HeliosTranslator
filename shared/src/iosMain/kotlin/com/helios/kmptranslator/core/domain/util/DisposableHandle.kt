15a3e49a0dd4c6ef9181dcab89a6a09a2c96065d
Duy Phạm <duypham@W3WMBP16A1044.local>
2023-11-26 21:47:21 +0700

    implement common flows

---
+++ shared/src/iosMain/kotlin/com/helios/kmptranslator/core/domain/util/DisposableHandle.kt
@@ -0,0 +1,3 @@
+package com.helios.kmptranslator.core.domain.util
+
+fun interface DisposableHandle : kotlinx.coroutines.DisposableHandle
---
+++ shared/src/iosMain/kotlin/com/helios/kmptranslator/core/domain/util/IOSMutableStateFlow.kt
@@ -0,0 +1,8 @@
+package com.helios.kmptranslator.core.domain.util
+
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class IOSMutableStateFlow<T>(
+    private val initialValue: T
+) : CommonMutableStateFlow<T>(MutableStateFlow(initialValue)) {
+}

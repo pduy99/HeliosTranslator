f6574b2ac29b08bbabd09415554acaaec077a410
Duy Phạm <duypham@W3WMBP16A1044.local>
2023-12-08 15:15:18 +0700

    add translate textfield

--- androidApp/src/main/java/com/helios/kmptranslator/android/translate/presentation/TranslateScreen.kt
+++ androidApp/src/main/java/com/helios/kmptranslator/android/translate/presentation/TranslateScreen.kt
@@ -1,26 +1,37 @@
 package com.helios.kmptranslator.android.translate.presentation
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
+import com.helios.kmptranslator.android.R
 import com.helios.kmptranslator.android.translate.presentation.components.LanguageDropDown
 import com.helios.kmptranslator.android.translate.presentation.components.SwapLanguagesButton
+import com.helios.kmptranslator.android.translate.presentation.components.TranslateTextField
 import com.helios.kmptranslator.translate.presentation.TranslateEvent
 import com.helios.kmptranslator.translate.presentation.TranslateState
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun TranslateScreen(
     state: TranslateState,
     onEvent: (TranslateEvent) -> Unit,
 ) {
+    val context = LocalContext.current
     Scaffold(
         floatingActionButton = {},
 
@@ -61,6 +72,39 @@ fun TranslateScreen(
                         }
                     )
                 }
+            }
+            item {
+                val clipboardManager = LocalClipboardManager.current
+                val keyboardController = LocalSoftwareKeyboardController.current
+
+                TranslateTextField(
+                    fromText = state.fromText,
+                    toText = state.toText,
+                    isTranslating = state.isTranslating,
+                    fromLanguage = state.fromLanguage,
+                    toLanguage = state.toLanguage,
+                    onTranslateClick = {
+                        keyboardController?.hide()
+                        onEvent(TranslateEvent.Translate)
+                    },
+                    onTextChanged = { onEvent(TranslateEvent.ChangeTranslationText(it)) },
+                    onCopyClick = {
+                        clipboardManager.setText(
+                            buildAnnotatedString {
+                                append(it)
+                            }
+                        )
+                        Toast.makeText(
+                            context,
+                            context.getString(R.string.copied_to_clipboard),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    },
+                    onCloseClick = { onEvent(TranslateEvent.CloseTranslation) },
+                    onSpeakerClick = { },
+                    onTextFieldClick = { onEvent(TranslateEvent.EditTranslation) },
+                    modifier = Modifier.fillMaxWidth()
+                )
             }
         }
 
